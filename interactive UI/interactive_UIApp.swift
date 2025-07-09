b705a37c9a4b267d5e348c71e0a2b103f699aa2a
anushka <anushka.vashisth02@gmail.com>
2025-07-09 14:32:53 -0500

    Initial Commit

---
+++ interactive UI/interactive_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  interactive_UIApp.swift
+//  interactive UI
+//
+//  Created by Scholar on 7/9/25.
+//
+
+import SwiftUI
+
+@main
+struct interactive_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ interactive UITests/interactive_UITests.swift
@@ -0,0 +1,17 @@
+//
+//  interactive_UITests.swift
+//  interactive UITests
+//
+//  Created by Scholar on 7/9/25.
+//
+
+import Testing
+@testable import interactive_UI
+
+struct interactive_UITests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
